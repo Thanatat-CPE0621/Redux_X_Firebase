1d4b1ceab4349bb88d547daae02c97fc144ace45
Lil-Fame <40058905+LilThanatat0621@users.noreply.github.com>
2019-04-22 17:35:15 +0700

    Navbar and CardBoard

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card -content grey-text tect-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd Septembder , 2m</p>
+                </div>
+            </div>
+
+            
+            <div className="card z-depth-0 project-summary">
+                <div className="card -content grey-text tect-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd Septembder , 2m</p>
+                </div>
+            </div>
+
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card -content grey-text tect-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd Septembder , 2m</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export  default ProjectList 
