6287672b744a5391e8e4d759fc37fa9bb3330fa5
Lil-Fame <40058905+LilThanatat0621@users.noreply.github.com>
2019-04-23 01:32:51 +0700

    Auth System without db

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProjectDetails = (props) => {
+
+   const id = props.match.params.id;
+  return (
+    <div className="container section project-detials">
+      <div className="card z-depth-0">
+        <div className="card-content">
+            <span className="card-title">Project Title {id}</span>
+            <p>Loram</p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+        <div>Posted bt rhe Net Ninja</div>
+        <div>2nd , September , 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,11 @@
 import React from 'react'
-
+import ProjectSummary  from './ProjectSummary'
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card -content grey-text tect-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd Septembder , 2m</p>
-                </div>
-            </div>
-
-            
-            <div className="card z-depth-0 project-summary">
-                <div className="card -content grey-text tect-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd Septembder , 2m</p>
-                </div>
-            </div>
-
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card -content grey-text tect-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd Septembder , 2m</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary /> 
         </div>
     )
 }
